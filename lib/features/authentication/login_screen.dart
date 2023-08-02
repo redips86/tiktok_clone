a8719b1b7d90dfed9889f93aaea628d594b85211
redips <redips@prismweaver.com>
2023-08-02 21:13:03 +0900

    [tiktok] AuthButton

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
@@ -31,7 +33,15 @@ class LoginScreen extends StatelessWidget {
                 'Create a profile, follow other accounts, make your own videos, and more.',
                 style: TextStyle(fontSize: Sizes.size16, color: Colors.black45),
                 textAlign: TextAlign.center,
-              )
+              ),
+              Gaps.v40,
+              AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.user),
+                  text: "Use email & password"),
+              Gaps.v16,
+              AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.apple),
+                  text: "Continue with Apple")
             ],
           ),
         ),
