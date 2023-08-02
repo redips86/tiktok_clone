80df4810ffdc15c90d231769395048ad9f536666
redips <redips@prismweaver.com>
2023-08-02 11:53:05 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/redips/tiktok_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.redips.tiktok_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+}
