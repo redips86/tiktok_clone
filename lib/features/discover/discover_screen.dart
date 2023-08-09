6a10df263b84d92a80596203acd393849e79e962
redips <redips@prismweaver.com>
2023-08-09 15:41:53 +0900

    [tiktok] SearchBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 final tabs = [
@@ -11,18 +14,45 @@ final tabs = [
   "Brands",
 ];
 
-class DiscoverScreen extends StatelessWidget {
+class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
+
+  @override
+  State<DiscoverScreen> createState() => _DiscoverScreenState();
+}
+
+class _DiscoverScreenState extends State<DiscoverScreen> {
+  final TextEditingController _textEditingController =
+      TextEditingController(text: "init text");
+
+  void _onSearchChagned(String value) {
+    print("Search value $value");
+  }
+
+  void _onSearchSubmitted(String value) {
+    print("Submitted value $value");
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 7,
       child: Scaffold(
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             elevation: 1,
             centerTitle: true,
-            title: const Text("discover"),
+            title: CupertinoSearchTextField(
+              controller: _textEditingController,
+              onChanged: _onSearchChagned,
+              onSubmitted: _onSearchSubmitted,
+            ),
             bottom: TabBar(
                 padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
                 splashFactory: NoSplash.splashFactory,
@@ -43,20 +73,84 @@ class DiscoverScreen extends StatelessWidget {
           body: TabBarView(
             children: [
               GridView.builder(
-                  itemCount: 20,
-                  padding: const EdgeInsets.all(Sizes.size6),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: Sizes.size10,
-                    mainAxisSpacing: Sizes.size10,
-                    childAspectRatio: 9 / 16,
-                  ),
-                  itemBuilder: (context, index) => Container(
-                        color: Colors.teal,
-                        child: Center(
-                          child: Text("$index"),
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                itemCount: 20,
+                padding: const EdgeInsets.all(Sizes.size6),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                  childAspectRatio: 9 / 21,
+                ),
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.size4)),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholderFit: BoxFit.cover,
+                          placeholder: 'assets/images/placeholder.jpeg',
+                          image:
+                              'https://source.unsplash.com/random/200x${355 + index}',
                         ),
-                      )),
+                      ),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      "This is a very long caption for my tiktok that i'm upload just now currently!",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: Sizes.size16 + Sizes.size2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v5,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.w600),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 30,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.black,
+                              foregroundColor: Colors.white,
+                              foregroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/104882161?v=4",
+                              ),
+                            ),
+                          ),
+                          Gaps.h4,
+                          const Expanded(
+                            child: Text(
+                              "My avatar is too long long long!!!",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h4,
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade500,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "2.5M",
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
               for (var tab in tabs.skip(1))
                 Center(
                   child: Text(
