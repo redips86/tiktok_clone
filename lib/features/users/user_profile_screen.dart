72d26ceeb736c12370715e39264b18aa69c90753
redips <redips@prismweaver.com>
2023-08-10 22:13:59 +0900

    [tiktok] SliverPersistentHeader

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -31,6 +31,15 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             title: const Text("Hello!"),
           ),
         ),
+        const SliverToBoxAdapter(
+            child: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.red,
+              radius: 20,
+            )
+          ],
+        )),
         SliverFixedExtentList(
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
@@ -43,6 +52,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               ),
             ),
             itemExtent: 100),
+        SliverPersistentHeader(
+          delegate: CustomDelegate(),
+          pinned: true,
+        ),
         SliverGrid(
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
@@ -62,5 +75,35 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ))
       ],
     );
+  }
+}
+
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+        color: Colors.indigo,
+        child: const FractionallySizedBox(
+            heightFactor: 1,
+            child: Center(
+              child: Text(
+                'Title!!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )));
+  }
+
+  @override
+  double get maxExtent => 100;
+
+  @override
+  double get minExtent => 50;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
